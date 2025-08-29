2ff15bbf103fd59df5e76762b456a96211864807
Giovane Alves <163547277+giovane453@users.noreply.github.com>
2025-08-29 08:47:39 -0300

    Add files via upload

--- mural.php
+++ mural.php
@@ -17,7 +17,7 @@ if(isset($_POST['cadastra'])){
 <html lang="pt-br">
 <head>
 <meta charset="utf-8"/>
-<title>Mural de pedidos</title>
+<title>Mural dos Veteranos</title>
 <link rel="stylesheet" href="style.css"/>
 <script src="scripts/jquery.js"></script>
 <script src="scripts/jquery.validate.js"></script>
@@ -42,7 +42,7 @@ $(document).ready(function() {
 <div id="main">
 <div id="geral">
 <div id="header">
-    <h1>Mural de pedidos</h1>
+    <h1>Mural de Veteranos</h1>
 </div>
 
 <div id="formulario_mural">
