ce707965340769283874865390bf026418d47410
Giovane Alves <163547277+giovane453@users.noreply.github.com>
2025-09-18 11:26:39 -0300

    Add files via upload

--- conexao.php
+++ conexao.php
@@ -15,54 +15,10 @@ if (!$conexao) {
 
 // SENSITIVE CASE suportar acentos e Ç
 mysqli_set_charset($conexao, "utf8");
-?>
-
-<?php
-// Configurações do banco de dados
-$host    = "localhost"; // servidor do banco (geralmente localhost)
-$usuario = "root";      // usuário do banco
-$senha   = " ";          // senha do banco
-$banco   = "mural3d";     // nome do banco de dados
-
-// Conexão MySQLi
-$conexao = mysqli_connect($host, $usuario, $senha, $banco);
-
-// Verifica se a conexão funcionou
-if (!$conexao) {
-    die("Erro ao conectar: " . mysqli_connect_error());
-}
-
-// Opcional: definir charset para evitar problemas com acentos
-mysqli_set_charset($conexao, "utf8");
-?>
-
-<?php
-// Configurações do banco de dados
-$host    = "localhost"; // servidor do banco
-$usuario = "root";      // usuário do banco
-$senha   = " ";          // senha do banco
-$banco   = " ";     // nome do banco de dados
-
-// Conexão MySQLi
-$conexao = mysqli_connect($host, $usuario, $senha, $banco);
-
-// Verifica se a conexão funcionou
-if (!$conexao) {
-    die("Erro ao conectar: " . mysqli_connect_error());
-}
-
-// Opcional: definir charset para evitar problemas com acentos
-mysqli_set_charset($conexao, "utf8");
-
-// ==========================================
-// A PARTIR DAQUI, CONFIGURAÇÕES DO CLOUDINARY
-// ==========================================
 
 // Substituam os valores abaixo pelas credenciais da sua própria conta do Cloudinary
-$cloud_name = "chave1";  // exemplo: "meucloud123"
+$cloud_name = "dhjpydls8";  // exemplo: "meucloud123"
 $api_key    = "993665631869481";     // exemplo: "123456789012345"
 $api_secret = "UbOrIOYnwYx46az_kMSpCHxFIHg";  // exemplo: "abcdeFGHijkLMNopqrstu"
 
 ?>
-
-
